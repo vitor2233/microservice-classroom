80005620eeba1ef459b05ed39f130380bcd05b6f
vitor2233 <vitorguilherme2233@gmail.com>
2024-03-23 10:00:08 -0300

    Get query with codegen

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,13 +1,20 @@
-import { getAccessToken, getSession, withApiAuthRequired, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useGetProductsQuery } from "@/graphql/generated/graphql";
+import { getServerPageGetProducts, ssrGetProducts } from "@/graphql/generated/page";
+import { withApollo } from "@/lib/withApollo";
+import { gql, useQuery } from "@apollo/client";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { GetServerSideProps } from "next";
 
-export default function Home() {
-    const { user } = useUser()
+function Home({ data }) {
+    const { user } = useUser();
 
     return (
         <div>
             <h1>Hello</h1>
+            <pre>
+                {JSON.stringify(data.products, null, 2)}
+            </pre>
             <pre>
                 {JSON.stringify(user, null, 2)}
             </pre>
@@ -15,10 +22,12 @@ export default function Home() {
     )
 }
 
-export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
+    getServerSideProps: async (ctx) => {
+        return getServerPageGetProducts({}, ctx)
+    }
+});
 
-/* PEGAR TOKEN async ({ req, res }) => {
-    const token = await getAccessToken(req, res);
-    console.log(token)
-    return { props: {} }
-}*/
+export default withApollo(
+    ssrGetProducts.withPage()(Home)
+);
