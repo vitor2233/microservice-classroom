b82fecc428e6a627880e1a470e73048f641f6962
vitor2233 <vitorguilherme2233@gmail.com>
2024-03-24 10:30:50 -0300

    Finish nextjs web

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { UserProvider } from "@auth0/nextjs-auth0";
 import type { AppProps } from "next/app";
 import '../styles/globals.css'
 
